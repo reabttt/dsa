7ffb4561ad3b0e988c6c952aa3c381c252fdd708
reabttt <joealcob05@gmail.com>
2023-05-22 19:12:05 -0300

    lista

--- List/lista.py
+++ List/lista.py
@@ -1,98 +1,175 @@
+def criterio_comparacion(value, criterio):
+    if isinstance(value, (int, str, bool)):
+        # print('es un primitivo')
+        return value
+    else:
+        # print('no es un dato primitivo')
+        dic_atributos = value.__dict__
+        if criterio in dic_atributos:
+            return dic_atributos[criterio]
+        else:
+            print('no se puede ordenar por este criterio')
+
+
 class Lista():
+
     def __init__(self):
         self.__elements = []
 
-    def __criterio(self, dato, criterio=None):
-        if criterio == None:
-            return dato
-        else:
-            return getattr(dato, criterio)
-
     def insertar(self, value, criterio=None):
-        if len(self.__elements) == 0:
+        # print('criterio de insercion', criterio)
+        if len(self.__elements) == 0 or criterio_comparacion(value, criterio) >= criterio_comparacion(self.__elements[-1], criterio):
             self.__elements.append(value)
-        elif self.__criterio(value, criterio) < self.__criterio(self.__elements[0], criterio):
+        elif criterio_comparacion(value, criterio) < criterio_comparacion(self.__elements[0], criterio):
             self.__elements.insert(0, value)
-        elif self.__criterio(value, criterio) >= self.__criterio(self.__elements[-1], criterio):
-            self.__elements.append(value)
         else:
             index = 1
-            while self.__criterio(value, criterio) >= self.__criterio(self.__elements[index], criterio):
+            while criterio_comparacion(value, criterio) >= criterio_comparacion(self.__elements[index], criterio):
                 index += 1
             self.__elements.insert(index, value)
 
+    
     def buscar(self, search_value, criterio=None):
         position = None
-        for index, value in enumerate(self.__elements):
-            if search_value == self.__criterio(value, criterio):
-                position = index
-                break
+        first = 0
+        last = self.tam() - 1
+        while (first <= last and position == None):
+            middle = (first + last) // 2
+            if search_value == criterio_comparacion(self.__elements[middle], criterio):
+                position = middle
+            elif search_value > criterio_comparacion(self.__elements[middle], criterio):
+                first = middle + 1
+            else:
+                last = middle - 1
         return position 
+
     
     def eliminar(self, value, criterio=None):
         return_value = None
-        pos = self.buscar(value, criterio)
+        pos = self.buscar(criterio_comparacion(value, criterio), criterio)
         if pos is not None:
             return_value = self.__elements.pop(pos)
-        return return_value 
+        return return_value
 
-    def tamano(self):
+    def tam(self):
         return len(self.__elements)
-    
-    def es_vacia(self):
-        return self.tamano() == 0
 
     def barrido(self):
         for value in self.__elements:
             print(value)
 
-    def elemento_valor(self, value, criterio=None):
-        return_value = None
-        pos = self.buscar(value, criterio)
-        if pos is not None:
-            return_value = self.__elements[pos]
-        return return_value
+    def reordenar(self, criterio=None):
+        def criterio_clave(valor):
+            return criterio_comparacion(valor, criterio) 
+        self.__elements.sort(key=criterio_clave)
+
+    # def get_element_by_value(self, value):
+    #     return_value = None
+    #     pos = self.search(value)
+
+    #     if pos is not None:
+    #         return_value = self.__elements[pos]
+    #     return return_value
 
     def elemento_indice(self, index):
         return_value = None
-        if index >= 0 and index < self.tamano():
+        if index >= 0 and index < self.tam():
             return_value = self.__elements[index]
         return return_value
 
-    def modificar_valor(self, value, new_value, criterio=None):
-        pos = self.buscar(value, criterio)
+    def modificar_valor(self, value, new_value):
+        pos = self.buscar(value)
         if pos is not None:
-            value = self.eliminar(value, criterio)
-            self.insertar(new_value, criterio)
-
+            value = self.eliminar(value)
+            self.insertar(new_value)
 
     # actividad 6 
-    def traje_armadua(self):
-        for value in self.__elements:
-            if 'traje' in value.biografia or 'armadura' in value.biografia:
-                print(f'{value.nombre}, {value.biografia}')
-
-    def aparicion_menor_1963(self):
-        for value in self.__elements:
-            if value.aparicion<1963:
-                print(f'{value.nombre} {value.casa}')
+    def eliminarLinternaVerde(self):
+        return self.eliminar('Linterna Verde','nombre')
     
-    def listar_bms(self):
-        for value in self.__elements:
-            if value.nombre[0] in ['b,m,s']:
-                print(value)
+    def aparicionWolverine(self):
+        node = self.elemento_indice(self.buscar('Wolverine','nombre'))
+        if node is not None:
+            return node.aparicion 
+        else:
+            return None
 
-    def cantidad_superheroes(self):
-        count_dc =0
-        count_marvel = 0
-        for value in self.__elements:
-            if value.casa == 'dc':
-                count_dc+=1 
+    def cambiarCasa(self):
+        node = self.elemento_indice(self.buscar('Dr. Strange','nombre'))
+        if node is not None:
+            node.casa = 'marvel'
+        else:
+            return None
+    
+    def armaduraTraje(self):
+        heroes = []
+        for node in self.__elements:
+            if 'traje' in node.biografia or 'armadura' in node.biografia:
+                heroes.append(node.nombre)
+        return heroes
+
+    def aparicion_1963(self):
+        heroes = []
+        for node in self.__elements:
+            if node.aparicion < 1963:
+                heroes.append((node.nombre, node.casa))
+        return heroes
+    
+    # le paso como parametro una lista con los nombres de los superheroes que estoy buscando
+    def mostrarCasaPersonajes(self,heroes):
+        casas_heroes = []
+        for heroe in heroes:
+            casa = self.elemento_indice(self.buscar(heroe,'nombre')).casa # devuelve el superheroe con el campo casa
+            if casa is not None:
+                casas_heroes.append((heroe,casa))
+        return casas_heroes
+
+    def infoFlshStarLord(self,heroes):
+        infos = []
+        for heroe in heroes:
+            node = self.elemento_indice(self.buscar(heroe,'nombre'))
+            if node is not None:
+                infos.append(node)
+        return infos
+
+    def inicialBMS(self):
+        heroes = []
+        for heroe in self.__elements:
+            if heroe.nombre[0] in ['B','M','S']:
+                heroes.append(heroe)
+        return heroes
+    
+    def ContarPorCasa(self):
+        contador = []
+        dc = 0
+        marvel = 0
+        for node in self.__elements:
+            if node.casa == 'dc':
+                dc += 1 
             else:
-                count_marvel+=1 
-        return count_dc,count_marvel
+                marvel += 1 
+        contador.append((dc,marvel))
+        return contador
 
+    # actividad 10 
+    def cancionMasLarga(self):
+        self.reordenar('duracion')
+        return self.elemento_indice(self.tam()-1)
+    
+    def topCanciones(self,top):
+        self.reordenar(criterio='reproducciones')
+        self.__elements.reverse()
+        return self.__elements[:top]
 
+    def cancionesBanda(self,artista):
+        canciones = []
+        for i in range(self.tam()):
+            node = self.elemento_indice(i)
+            if node.artista == artista:
+                canciones.append(node)
+        return canciones 
+
+    
 
 
 
--- List/prueba.py
+++ List/prueba.py
@@ -1,4 +1,5 @@
-from lista import Lista
+from lista import Lista 
+import random 
 
 class Superheroe:
     def __init__(self,nombre,aparicion,casa,biografia):
@@ -18,14 +19,14 @@ superheroes = [
         {'name':'star lord','year':1986,'comic':'marvel','bio':'...'},
         {'name':'flash','year':1940,'comic':'dc','bio':'el velocista escarlata'},
         {'name':'mujer maravilla','year':1941,'comic':'dc','bio':'guerrera de las amazonas'},
-        {'name':'dr. doom','year':1962,'comic':'marvel','bio':'genio inventor'},
+        {'name':'deadpool','year':1991,'comic':'marvel','bio':'...'},
         {'name':'capitana marvel','year':1968,'comic':'marvel','bio':'traje azul y rojo'}, 
-        {'name':'iron man','year':1963,'comic':'dc','bio':'armadura de alta tecnologia'},
+        {'name':'iron man','year':1963,'comic':'marvel','bio':'armadura de alta tecnologia'},
         ]
 
 lista = Lista()
 for valor in superheroes:
-    lista.insertar(Superheroe(valor['name'],
+    lista.insertar(Superheroe(valor['name'].title(),
                               valor['year'],
                               valor['comic'],
                               valor['bio']),'nombre')
@@ -34,29 +35,103 @@ lista.barrido()
 print('')
 
 # punto a 
-nodo = lista.eliminar('linterna verde','nombre')
-if nodo:
-    print(f'se elimino a {nodo.nombre}')
+data = lista.eliminarLinternaVerde()
+if data is not None:
+    print('se elimino Linterna Verde')
 else:
-    print('{nodo.nombre} no esta en la lista')
+    print('Linterna Verde no esta en la lista')
 # punto b 
-nodo = lista.elemento_valor('wolverine','nombre')
-if nodo:
-    print(f'primera aparicion de {nodo.nombre} {nodo.aparicion}')
-else:
-    print(f'wolverine no esta en la lista')
+data = lista.aparicionWolverine()
+print(f'primera aparicion de Wolverine {data}') 
 # punto c 
-nodo = lista.elemento_valor('dr. strange','nombre')
-if nodo:
-    nodo.casa = 'marvel'
-    print(f'{nodo.nombre} ahora esta en marvel')
-else:
-    print(f'dr. strange no esta en la lista')
+data = lista.cambiarCasa()
+print('se cambio la casa comic de Dr. Strange\n')
+lista.barrido()
 # punto d 
-print('\ntiene la palabra traje o armadura en la bio')
-lista.traje_armadua()
+print('\nla palabra traje o armadura aparece en la bio')
+data = lista.armaduraTraje()
+for valor in data:
+    print(valor)
+# punto e 
+print('\naño de aparicion anterior a 1963')
+data = lista.aparicion_1963()
+for valor in data:
+    print(valor[0], valor[1])
+# punto f 
+print('')
+heroes = ['Capitana Marvel','Mujer Maravilla']
+data = lista.mostrarCasaPersonajes(heroes)
+for heroe, casa in data:
+    print('{} es de la casa {}'.format(heroe,casa))
+# punto g
+print('')
+info = ['Flash','Star Lord']
+data = lista.infoFlshStarLord(info)
+for heroe in data:
+    print(heroe)
+# punto h
+print('\nsuperheroes cuyas iniciales son B M o S')
+data = lista.inicialBMS()
+for valor in data:
+    print(valor)
+# punto i 
+data = lista.ContarPorCasa()
+for valor in data:
+    print(f'\nsuperheroes de dc {valor[0]}')
+    print(f'superheroes de marvel {valor[1]}')
 
 
+
+class Cancion:
+    def __init__(self,titulo,artista,duracion,reproducciones):
+        self.titulo = titulo
+        self.artista = artista
+        self.duracion = duracion
+        self.reproducciones = reproducciones
+    
+    def __str__(self):
+        return f'{self.titulo}  {self.artista}  {self.duracion}  {self.reproducciones}'
+        
+lista_canciones = Lista()
+
+canciones = [
+            {'title':'do i wannna know?','art':'artic monkeys','dur':'4:25','rep':random.randint(1,100)},
+            {'title':'cancion 1','art':'artista 1','dur':'3:00','rep':random.randint(1,100)},       
+            {'title':'cancion 2','art':'artista 2','dur':'1:33','rep':random.randint(1,100)},      
+            {'title':'cancion 3','art':'artista 3','dur':'3:45','rep':random.randint(1,100)},      
+            {'title':'cancion 4','art':'artista 4','dur':'2:05','rep':random.randint(1,100)},      
+            {'title':'cancion 5','art':'artista5','dur':'5:00','rep':random.randint(1,100)},       
+            {'title':'cancion 6','art':'artista 6','dur':'3:06','rep':random.randint(1,100)},       
+            {'title':'cancion 7','art':'artista 7','dur':'1:07','rep':random.randint(1,100)},      
+            {'title':'cancion 8','art':'artista 8','dur':'4:45','rep':random.randint(1,100)},      
+            {'title':'cancion 9','art':'artista9','dur':'2:00','rep':random.randint(1,100)},       
+        ]
+
+print('\nlista de canciones')
+for cancion in canciones:
+    lista_canciones.insertar(Cancion(cancion['title'].title(),
+                           cancion['art'].title(),
+                           cancion['dur'],
+                           cancion['rep']),'titulo')
+
+lista_canciones.barrido()
+# punto a 
+data = lista_canciones.cancionMasLarga()
+print(f'\ncancion mas larga {data}') 
+# punto b
+print('\ntop 5 de canciones mas escuchadas')
+data = lista_canciones.topCanciones(5)
+for cancion in data:
+    print(cancion)
+print('\ntop 10 de canciones mas escuchadas')
+data = lista_canciones.topCanciones(10)
+for cancion in data:
+    print(cancion)
+# punrto c
+print('\ncancones de Artic Monkeys')
+data = lista_canciones.cancionesBanda('Artic Monkeys')
+for cancion in data:
+    print(cancion)
 
 
 
